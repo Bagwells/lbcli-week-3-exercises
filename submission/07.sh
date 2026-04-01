ddb3d201130c6f6ee2dcc367ee1bc5bb72a314f1
Bagwells <timmyayomidebagwells@outlook.com>
2026-04-01 07:33:20 +0100

    Updated

--- submission/07.sh
+++ submission/07.sh
@@ -3,4 +3,4 @@
 
 tx="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
 
-bitcoin-cli -regtest decodepsbt "$tx" | jq '[.tx.vout[].value] | tonumber * 100000000 | add | floor'
+bitcoin-cli -regtest decodepsbt "$tx" | jq '.tx.vout[].value | tonumber * 100000000 | add | floor'
