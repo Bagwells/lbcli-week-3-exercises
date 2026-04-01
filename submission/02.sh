4ccf2a12ca82844f75a76742f0aefd46e7f2d6c6
Bagwells <timmyayomidebagwells@outlook.com>
2026-04-01 04:29:39 +0100

    partial submission entry

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,7 @@
 # Create a native segwit address and get the public key from the address.
+
+nativeSegwitAddress=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" "bech32")
+
+pubKey=$(bitcoin-cli -regtest -rpcwallet=builderswallet getaddressinfo $nativeSegwitAddress | jp -r '.pubkey')
+
+echo $pubKey
